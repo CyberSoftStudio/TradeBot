875d135193eea626c2f8c35e0958a1f71307c958
vulfke <vulfke@gmail.com>
2018-09-06 18:00:38 +0300

    Added some code to Monitor_bitmex

--- Monitor_bitmex.py
+++ Monitor_bitmex.py
@@ -1,11 +1,16 @@
 import json
+import datetime
+
 
 class Monitor_bitmex:
     def __init__(self, client):
         self.client = client
 
-    def get_quote(self):
-        pass
+    def get_quote(self, symbol, startTime, endTime=None, reverse=False, count=50):
+        if endTime is None:
+            endTime = datetime.datetime.utcnow()
+
+        return self.client.Quote.Quote_get(symbol=symbol, startTime=startTime, endTime=endTime, reverse=reverse, count=count).result()
 
     def get_instrument(self):
         pass
