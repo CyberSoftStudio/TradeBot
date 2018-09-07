8e6b33aa7ce4c56ed7dd523bad1f25f5202fe0b6
Vulfke <vulfke@gmail.com>
2018-09-07 19:08:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from Bot import Bot
 import time
 
-
-auth_info = {
-    'api_public': 'M9C8G0sR_xyAH-P-McPtGtFU',
-    'api_secret': 'virblf2ij2unSG8VNuwsq5VTw0axnIVYwodKlgeAO0wGqVBS'
-}
+auth_info = {}
 
 bot = Bot(auth_info, test=False)
 
